2110a43d68b7b74737926662b7f1545ef6f66479
wxie <wxie@cisco.com>
2022-04-07 20:11:32 +0800

    更新clash脚本

--- ql/clash_check.py
+++ ql/clash_check.py
@@ -11,12 +11,9 @@ def get_all_proxies():
     proxy_api = 'http://192.168.31.1:9999/proxies'
     data = requests.get(url=proxy_api).text
     proxies = json.loads(data).get('proxies')
-    count = 0
     for key, value in proxies.items():
         if key == '♻️ 自动选择':
             for proxy in value.get('all')[:-1]:
-                print("开始处理第 " + str(count) + "个请求...")
-                print("proxy: " + proxy)
                 api = base_url + quote(proxy) + args
                 requests.get(url=api)
     print('Healthy check end.')
