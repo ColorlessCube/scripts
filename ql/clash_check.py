b416d60f41ad9eed91184232a201cf53d654f19a
wxie <wxie@cisco.com>
2022-04-07 18:54:38 +0800

    添加clash脚本

---
+++ ql/clash_check.py
@@ -0,0 +1,23 @@
+import json
+from urllib.parse import quote
+import requests
+
+base_url = 'http://192.168.31.1:9999/proxies/'
+args = '/delay?timeout=5000&url=http://www.gstatic.com/generate_204'
+
+
+def get_all_proxies():
+    print('Healthy check start.')
+    proxy_api = 'http://192.168.31.1:9999/proxies'
+    data = requests.get(url=proxy_api).text
+    proxies = json.loads(data).get('proxies')
+    for key, value in proxies.items():
+        if key == '♻️ 自动选择':
+            for proxy in value.get('all')[:-1]:
+                api = base_url + quote(proxy) + args
+                requests.get(url=api)
+    print('Healthy check end.')
+
+
+if __name__ == '__main__':
+    get_all_proxies()
